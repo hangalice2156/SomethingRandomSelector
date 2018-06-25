1408f575e76667f3d689e21d4faa3543fd83e29d
cicero884 <cicero884@gmail.com>
2018-06-25 17:03:03 +0800

    add generater

--- SomethingRandomGenerator/SomethingRandomGenerator/Form_generate.Designer.cs
+++ SomethingRandomGenerator/SomethingRandomGenerator/Form_generate.Designer.cs
@@ -29,10 +29,72 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label_main = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_main
+            // 
+            this.label_main.AutoSize = true;
+            this.label_main.Location = new System.Drawing.Point(11, 8);
+            this.label_main.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_main.Name = "label_main";
+            this.label_main.Size = new System.Drawing.Size(53, 12);
+            this.label_main.TabIndex = 4;
+            this.label_main.Text = "現有表單";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 22);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(600, 153);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(131, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            // 
+            // Form_generate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 505);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_main);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form_generate";
             this.Text = "Form_generate";
+            this.Load += new System.EventHandler(this.Form_generate_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_main;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- SomethingRandomGenerator/SomethingRandomGenerator/Form_generate.cs
+++ SomethingRandomGenerator/SomethingRandomGenerator/Form_generate.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 namespace SomethingRandomGenerator
 {
     public partial class Form_generate : Form
@@ -18,7 +20,61 @@ namespace SomethingRandomGenerator
         {
             InitializeComponent();
         }
+        DataSet ds;
+        SqlConnection db = new SqlConnection();
 
+        private void Form_generate_Load(object sender, EventArgs e)
+        {
+            // TODO: 這行程式碼會將資料載入 'database1DataSet.MainControl' 資料表。您可以視需要進行移動或移除。
+            string cn = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+            "AttachDbFilename=|DataDirectory|Database1.mdf;" +
+            "Integrated Security=True";
+            db.ConnectionString = cn;
+            //int row_count = 0;
+            ds = new DataSet();
+            SqlDataAdapter daMain = new SqlDataAdapter("SELECT * FROM MainControl", cn);
+            daMain.Fill(ds, "MainControl");
 
+            /*SqlDataAdapter daEat = new SqlDataAdapter("SELECT * FROM 東西吃啥", cn);
+            daEat.Fill(ds, "東西吃啥");
+            SqlDataAdapter daStudent = new SqlDataAdapter("SELECT * FROM 學生名單", cn);
+            daStudent.Fill(ds, "學生名單");*/
+            int row_count=ds.Tables[0].Rows.Count;
+            //foreach (DataRow row in ds.Tables[0].Rows) row_count++;
+
+            DataTableReader dtr = new DataTableReader(ds.Tables[0]);
+            for (int i = 0; i < row_count; ++i)
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM " + ds.Tables[0].Rows[i]["Name"], cn);
+                da.Fill(ds, "" + ds.Tables[0].Rows[i]["Name"]);
+            }
+
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "MainControl";
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            control_index = e.RowIndex + 1;
+            column_count = ds.Tables[control_index].Columns.Count;
+            //foreach (DataRow row in ds.Tables[control_index].Rows) row_count++;
+            //row_count = ds.Tables[control_index].Rows.Count;
+            times=rnd.Next(30);
+            timer1.Tick += change;
+            //MessageBox.Show("" + + ds.Tables[control_index].Rows[rnd.Next(3)][1]+ ds.Tables[control_index].Rows[rnd.Next(3)][2]);
+        }
+        Random rnd = new Random(Guid.NewGuid().GetHashCode());
+        int control_index, column_count,times;
+        private void change(object sender,EventArgs e)
+        {
+            if (times == 0) timer1.Tick -= change;
+            int rnd_out = rnd.Next(ds.Tables[control_index].Rows.Count);
+            string output = "";
+            for (int i = 1; i < column_count; ++i)
+            {
+                output += ds.Tables[control_index].Rows[rnd_out][i] + " ";
+            }
+            label1.Text = output;
+            times--;
+        }
     }
 }
